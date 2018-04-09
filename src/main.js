79392d83e97a065dda3af63523ea5d12e773f578
jsmond2016 <jsmond2016@gmail.com>
2018-04-09 09:57:52 +0800

    add components

--- src/main.js
+++ src/main.js
@@ -1,15 +1,26 @@
 import Vue from 'vue'
 import App from './App'
-import router from './router'
 import VueRouter from 'vue-router'
+import goods from './components/goods/goods'
+import ratings from './components/ratings/ratings'
+import seller from './components/seller/seller'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+
+let routes = [
+  { path: '/goods', component: goods },
+  { path: '/ratings', component: ratings },
+  { path: '/seller', component: seller}
+]
+
+let router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
 })
+
+let app = new Vue({
+  router
+}).$mount('#app')
