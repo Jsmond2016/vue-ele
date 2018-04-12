d3d76ba59e9d9f2976888e6f2f9c1b2c03cbc207
jsmond2016 <jsmond2016@gmail.com>
2018-04-12 09:51:31 +0800

    save code

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -5,6 +5,11 @@
 </template>
 
 <script type="text/ecmascript-6">
+  const LENGTH = 5
+  const CLS_ON = 'on'
+  const CLS_HALF = 'half'
+  const CLS_OFF = 'off'
+
     export default {
         props: {
           size: {
@@ -17,6 +22,12 @@
       compted: {
           starType() {
             return 'star-' + this.size
+          },
+          itemClasses() {
+            let result = []
+            let score = MATH.floor(5this.score * 2) / 2
+            let hashDecimal = score % 1 !=0
+            let intege =
           }
       }
     }
