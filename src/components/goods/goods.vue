7f8104766f66448b893b45ac05955188e6840d0d
jsmond2016 <jsmond2016@gmail.com>
2018-04-12 20:42:45 +0800

    完成7-1

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,48 @@
 <template>
-<div>i am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {}
+    const ERR_OK = 0
+
+    export default {
+      props: {
+        seller: {
+          type: Object
+        }
+      },
+      data() {
+        return {
+          goods: []
+        }
+      },
+      created() {
+        this.$http.get('/api/goods').then((response) => {
+          response = response.body
+          if (response.errno === ERR_OK) {
+            this.goods = response.data
+            console.log(this.goods)
+          }
+        })
+      }
+    }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+
+  .goods
+    display: flex
+    position: absolute
+    top: 174px
+    bottom: 46px
+    width: 100%
+    overflow: hidden
+    .menu-wrapper
+      flex: 0 0 80px
+      width: 80px /*这里不写安卓浏览器*/
+      background: #f3f5f7
 
 </style>
