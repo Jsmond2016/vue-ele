a2bd23ee9945c6273b93f9c26f329c748b22cdf9
jsmond2016 <jsmond2016@gmail.com>
2018-04-14 00:23:40 +0800

    完成购物车结算2中状态

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -12,8 +12,8 @@
           <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
         </div>
         <div class="content-right">
-          <div class="pay">
-            起送费￥{{minPrice}}元
+          <div class="pay" :class="payClass">
+            {{payDesc}}
           </div>
         </div>
       </div>
@@ -26,7 +26,12 @@
         selectFoods: {
           type: Array,
           default() {
-            return []
+            return [
+              {
+                price: 10,
+                count: 5
+              }
+            ]
           }
         },
         deliveryPrice: {
@@ -53,6 +58,23 @@
             count += food.count
           })
           return count
+        },
+        payDesc() {
+          if (this.totalPrice === 0) {
+            return `￥${this.minPrice}元起送`
+          } else if (this.totalPrice < this.minPrice) {
+            let diff = this.minPrice - this.totalPrice
+            return `还差￥${diff}元起送`
+          } else {
+            return '去结算'
+          }
+        },
+        payClass() {
+          if (this.totalPrice < this.minPrice) {
+            return 'not-enough'
+          } else {
+            return 'enough'
+          }
         }
       }
     }
@@ -143,5 +165,10 @@
             text-align: center
             font-size: 12px
             font-weight: 700
+            color: white
             background: #2b343b
+            &.not-enough
+              background: #2b343b
+            &.enough
+              background: #00b43c
 </style>
