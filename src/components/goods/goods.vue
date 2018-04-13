058c56a2e96dad45a0d263d4f5751e48e3808031
jsmond2016 <jsmond2016@gmail.com>
2018-04-13 17:43:58 +0800

    修复 span 间距问题

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -23,12 +23,10 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -158,7 +156,7 @@
         .desc
           margin-bottom: 8px
         .extra
-          &.count
+          .count
             margin-right: 12px
         .price
           font-weight: 700
