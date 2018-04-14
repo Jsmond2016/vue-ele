e6c49c37ae8feda2b4726a1a7e63d12c3b32c02f
jsmond2016 <jsmond2016@gmail.com>
2018-04-14 11:45:20 +0800

    完成 7-16 按钮添加和减少逻辑

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="cartcontrol">
+      <div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
+      <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
+      <div class="cart-add icon-add_circle" @click="addCart"></div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Vue from 'vue'
+    export default {
+      props: {
+        food: {
+          type: Object
+        }
+      },
+      methods: {
+        addCart() {
+          if (!this.food.count) {
+            Vue.set(this.food, 'count', 1)
+          } else {
+            this.food.count++
+          }
+          console.log(this.food.count)
+        },
+        decreaseCart() {
+          if (this.food.count) {
+            this.food.count--
+          }
+        }
+      }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .cartcontrol
+    font-size: 0
+    .cart-decrease
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0,160,220)
+    .cart-count
+      display: inline-block
+      vertical-align: top
+      width: 12px
+      height: 12px
+      line-height:  24px
+      padding-top: 6px
+      text-align: center
+      font-size:  10px
+      color: rgb(147,153,159)
+    .cart-add
+      display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0,160,220)
+</style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -28,6 +28,11 @@
                 <div class="price">
                   <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
                 </div>
+                <div class="cartcontrol-wrapper">
+                  <cartcontrol :food="food">
+
+                  </cartcontrol>
+                </div>
               </div>
             </li>
           </ul>
@@ -41,6 +46,7 @@
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll'
   import shopcart from 'components/shopcart/shopcart'
+  import cartcontrol from 'components/cartcontrol/cartcontrol'
 
     const ERR_OK = 0
 
@@ -65,6 +71,7 @@
           })
 
           this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+            click: true,
             probeType: 3
           })
 
@@ -116,7 +123,8 @@
         })
       },
       components: {
-        shopcart
+        shopcart,
+        cartcontrol
       }
     }
 </script>
@@ -223,4 +231,10 @@
             text-decoration: line-through
             font-size: 10px
             color: rgb(147,153,159)
+
+        .cartcontrol-wrapper
+          position: absolute
+          right: 0
+          bottom: 12px
+
 </style>
