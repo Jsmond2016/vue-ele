5b6c15157fca280c6cfd6b5e65dc82cd14678683
jsmond2016 <jsmond2016@gmail.com>
2018-04-15 23:03:50 +0800

    完成 8-1

---
+++ src/components/food/food.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="food" v-show="showFlag=false">
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+      props: {
+        food: {
+          type: Object
+        }
+      }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position: fixed
+    left: 0
+    top: 0
+    bottom: 48px
+    z-index: 30
+    width: 100%
+</style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -15,7 +15,7 @@
         <li v-for="(item,index) in goods" class="food-list food-list-hook" :key="index" >
           <h1 class="title">{{item .name}}</h1>
           <ul>
-            <li v-for="(food,index) in item.foods" class="food-item border-1px" :key="index">
+            <li v-for="(food,index) in item.foods" class="food-item border-1px" :key="index" @click="selectFood(food,$event)">
               <div class="icon">
                 <img :src="food.icon" width="57px" height="57px">
               </div>
@@ -40,6 +40,7 @@
       </ul>
     </div>
     <shopcart ref="shopcart" :select-foods="selectFoods" :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
+    <food :food="selectedFood"></food>
   </div>
 </template>
 
@@ -47,6 +48,7 @@
   import BScroll from 'better-scroll'
   import shopcart from 'components/shopcart/shopcart'
   import cartcontrol from 'components/cartcontrol/cartcontrol'
+  import food from 'components/food/food'
 
     const ERR_OK = 0
 
@@ -60,8 +62,8 @@
         return {
           goods: [],
           listHeight: [],
-          scrollY: 0
-
+          scrollY: 0,
+          selectedFood: {}
         }
       },
       methods: {
@@ -96,6 +98,12 @@
         },
         _drop(target) {
           this.$refs.shopcart.drop(target)
+        },
+        selectFood(food, event) {
+          if (!event._constructed) {
+            return 0
+          }
+          this.selectedFood = food
         }
       },
       computed: {
@@ -138,7 +146,8 @@
       },
       components: {
         shopcart,
-        cartcontrol
+        cartcontrol,
+        food
       },
       events: {
         'cart.add'(target) {
