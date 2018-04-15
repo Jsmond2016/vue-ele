84581b46da3babe88032ca2a14226a664cedf7fb
jsmond2016 <jsmond2016@gmail.com>
2018-04-15 15:23:12 +0800

    简单完成购物车列表css

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="shopcart">
-      <div class="content">
+      <div class="content" @click="toggleList">
         <div class="content-left">
           <div class="logo-wrapper">
             <div class="logo" :class="{'highlight': totalCount>0}">
@@ -25,12 +25,35 @@
             </transition>
           </div>
         </div>
+        <transition name="fold">
+          <div class="shopcart-list" v-show="listShow">
+            <div class="list-header">
+              <h1 class="title">购物车</h1>
+              <span class="empty">清空</span>
+            </div>
+            <div class="list-content">
+              <ul>
+                <li class="food" v-for="(food,index) in selectFoods" :key="index">
+                  <span class="name">{{food.name}}</span>
+                  <div class="price">
+                    <span>￥{{food.price*food.count}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    <cartcontrol :food="food"></cartcontrol>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </transition>
       </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
-    export default {
+  import cartcontrol from 'components/cartcontrol/cartcontrol'
+
+  export default {
       props: {
         selectFoods: {
           type: Array,
@@ -61,7 +84,8 @@
             {show: false},
             {show: false}
           ],
-          dropBalls: []
+          dropBalls: [],
+          fold: true
         }
       },
       methods: {
@@ -112,6 +136,12 @@
             ball.show = false
             el.style.display = 'none'
           }
+        },
+        toggleList() {
+          if (!this.totalCount) {
+            return 0
+          }
+          this.fold = !this.fold
         }
       },
 
@@ -146,12 +176,29 @@
           } else {
             return 'enough'
           }
+        },
+        listShow: {
+          get: function() {
+            return this.fold
+          },
+          set: function() {
+            if (!this.totalCount) {
+              this.fold = true
+              return false
+            }
+            let show = !this.fold
+            return show
+          }
         }
+      },
+    components: {
+        cartcontrol
       }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
     .shopcart
       position: fixed
       left: 0
@@ -255,4 +302,55 @@
               border-radius: 50%
               background: rgb(0,160,220)
               transition: all .4s linear
+        .shopcart-list
+          position: absolute
+          top: 0
+          left:  0
+          z-index: -1
+          width: 100%
+          transition: all .3s
+          transform: translate3d(0, -100%, 0)
+          &.fold-enter-active, &.fold-leave-active
+            transform: translate3d(0, 0, 0)
+          .list-header
+            height: 40px
+            line-height: 40px
+            padding: 0 18px
+            background: #f3f5f7
+            border-bottom: 1px solid rgba(7,17,27,0.1)
+            .title
+              float: left
+              font-size: 14px
+              color: rgb(7,17,27)
+            .empty
+              float: right
+              font-size: 12px
+              color: rgb(0,160,220)
+          .list-content
+            padding: 0 18px
+            max-height: 217px
+            background: #fff
+            overflow: hidden
+            .food
+              position: relative
+              padding: 12px
+              box-sizing: border-box
+              border-1px(rgba(7,17,27,0.1))
+              .name
+                line-height: 24px
+                font-size: 14px
+                color: rgb(7,17,27)
+              .price
+                position: absolute
+                right: 90px
+                bottom: 12px
+                line-height: 24px
+                font-size: 14px
+                font-weight: 700
+                color: rgb(240,20,20)
+              .cartcontrol-wrapper
+                position: absolute
+                right: 0
+                bottom: 8px
+
 </style>
