62a6df0159420dd94b84e3dcd2d11d0bb8ceadc2
jsmond2016 <jsmond2016@gmail.com>
2018-04-18 13:04:38 +0800

    完成 9-5

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -11,7 +11,7 @@
           <div class="score-wrapper">
             <span class="title4">服务态度</span>
             <star :size="36" :score="seller.serviceScore"></star>
-            <span class="score">{{seller.service}}</span>
+            <span class="score">{{seller.serviceScore}}</span>
           </div>
           <div class="score-wrapper">
             <span class="title4">商品评分</span>
@@ -24,25 +24,73 @@
           </div>
         </div>
       </div>
+      <split></split>
+      <ratingselect :select-type="selectType" :desc="desc" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="(rating,index) in ratings" :key="index" class="rating-item">
+            <div class="avatar">
+              <img :src="rating.avatar" width="28" height="28">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span class="item" v-for="item in rating.recommend">{{item}}</span>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import star from 'components/star/star'
-    export default {
+  import split from 'components/split/split'
+  import ratingselect from 'components/ratingselect/ratingselect'
+
+  const ALL = 2
+  const ERR_OK = 0
+
+  export default {
       props: {
         seller: {
           type: Object
         }
       },
+      data() {
+        return {
+          ratings: [],
+          selectType: ALL,
+          onlyContent: true,
+        }
+      },
+    created()  {
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data
+        }
+      })
+    },
       components: {
-        star
+        star,
+        split,
+        ratingselect
       }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+
   .ratings
     position: absolute
     top: 174px
@@ -59,6 +107,9 @@
         width: 137px
         border-right: 1px solid rgba(7,17,27,0.1)
         text-align: center
+        @media only screen and (max-width: 320px)
+          flex: 0 0 120px
+          width: 120px
         .score
           margin-bottom: 6px
           line-height: 28px
@@ -76,7 +127,9 @@
           color: rgb(147,153,159)
       .overview-right
         flex: 1
-        pading-left: 24px
+        padding:6px 0  6px 24px
+        @media only screen and (max-width: 320px)
+          padding-left: 6px
         .score-wrapper
           margin-bottom: 8px
           font-size: 0
@@ -90,6 +143,10 @@
             display: inline-block
             vertical-align: top
             margin: 0 12px
+            @media only screen and (max-width: 320px)
+              margin: 0 6px
+            .star-item
+              margin-right: 6px
           .score
             display: inline-block
             line-height: 18px
@@ -106,4 +163,59 @@
             margin-left: 12px
             font-size: 12px
             color: rgb(147,153,159)
+    .rating-wrapper
+      padding: 0 18px
+      .rating-item
+        display: flex
+        padding: 18px 0
+        border-1px(rgba(7,17,27,0.1))
+        .avatar
+          flex: 0 0 28px
+          width: 28px
+          margin-rignt: 12px
+          img
+            border-radius: 50%
+        .content
+          position: relative
+          flex: 1
+          .name
+            margin-bottom:  4px
+            line-height: 12px
+            font-size: 10px
+            color: rgb(7,17,27)
+          .star-wrapper
+            margin-bottom: 6px
+            font-size: 0
+            .star
+              display: inline-block
+              margin-right: 6px
+              vertical-align: top
+            .delivery
+              display: inline-block
+              vertical-align: top
+              line-height: 12px
+              font-size: 10px
+              color: rgb(147, 153, 159)
+
+
+          .text
+            margin-bottom: 8px
+            line-height: 18px
+            color: rgb(7,17,27)
+            font-size: 12px
+          .recommend
+            line-height: 16px
+            .icon-thumb_up, .item
+              display: inline-block
+              margin: 0 8px 4px 0
+              font-size: 9px
+            .icon-thumb_up
+              color: rgb(0, 160, 220)
+            .item
+              padding: 0 6px
+              border: 1px solid rgba(7, 17, 27, 0.1)
+              border-radius: 1px
+              color: rgb(147, 153, 159)
+              background: #fff
+
 </style>
