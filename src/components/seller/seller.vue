d0be89db44d633a9ad26811af663c6b5395e51d4
jsmond2016 <jsmond2016@gmail.com>
2018-04-18 22:51:17 +0800

    添加seller公告部分内容

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -1,24 +1,24 @@
 <template>
-  <div class="seller">
+  <div class="seller" ref="seller">
     <div class="seller-content">
       <div class="overview">
         <h1 class="title6">{{seller.name}}</h1>
         <div class="desc">
           <star :size="36" :score="seller.score"></star>
-          <span class="text">{{seller.ratingCount}}</span>
-          <span class="text">{{seller.sellCount}}</span>
+          <span class="text">({{seller.ratingCount}})</span>
+          <span class="text">月售{{seller.sellCount}}单</span>
         </div>
         <ul class="remark">
           <li class="block">
             <h2>起送价</h2>
             <div class="content">
-              <span class="stress">{{seller.minPrice}}元</span>
+              <span class="stress">{{seller.minPrice}}</span>元
             </div>
           </li>
           <li class="block">
             <h2>商家配送</h2>
             <div class="content">
-              <span class="stress">{{seller.deliveryPrice}}元</span>
+              <span class="stress">{{seller.deliveryPrice}}</span>元
             </div>
           </li>
           <li class="block">
@@ -29,24 +29,184 @@
           </li>
         </ul>
       </div>
+      <split></split>
+      <div class="bulletin">
+        <h1 class="title7">公告与活动</h1>
+        <div class="content-wrapper border-1px">
+          <p class="content">{{seller.bulletin}}</p>
+        </div>
+        <ul class="supports" v-if="seller.supports">
+          <li class="support-item border-1px" v-for="(item,index) in seller.supports" :key="index">
+            <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+            <span class="text">{{seller.supports[index].description}}</span>
+          </li>
+        </ul>
+      </div>
+      <split></split>
+      <div class="pics">
+        <h1 class="title">商家实景</h1>
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
+            <li class="pic-item" v-for="(pic,index) in seller.pics" :key="index">
+              <img :src="pic" width="120" height="90">
+            </li>
+          </ul>
+        </div>
+      </div>
+      <split></split>
+      <div class="info">
+        <h1 class="title border-1px">商家信息</h1>
+        <ul>
+          <li class="info-item" v-for="(info,index) in seller.infos" :key="index">{{info}}</li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import star from 'components/star/star'
+  import split from 'components/split/split'
+  import BScroll from 'better-scroll'
     export default {
       props: {
         seller: {
           type: Object
         }
       },
+      created() {
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      },
+      mounted() {
+        this.$nextTick(() => {
+          this._initScroll()
+          this._initPics()
+        })
+      },
+      watch: {
+        'seller'() {
+          this._initScroll()
+        }
+      },
+      methods: {
+        _initScroll() {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.seller, {
+              click: true
+            })
+          } else {
+            this.scroll.refresh()
+          }
+        },
+        _initPics() {
+
+        }
+      },
       components: {
-        star
+        star,
+        split
       }
     }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
+  .seller
+    position: absolute
+    top: 174px
+    bottom: 0
+    left: 0
+    width: 100%
+    overflow: hidden
+    .overview
+       padding: 18px
+       .title6
+         margin-bottom: 8px
+         line-height: 14px
+         color: rgb(7,17,27)
+         font-size: 14px
+       .desc
+         padding-bottom: 18px
+         line-height: 18px
+         border-1px(rgba(7,17,27,0.1))
+         .star
+           display: inline-block
+           margin-right: 8px
+           vertical-align: top
+           .star-item
+              margin-right: 6px
+              &:last-child
+                margin-right: 0
+         .text
+           display: inline-block
+           margin-right: 12px
+           line-height: 18px
+           vertical-align: top
+           font-size: 10px
+           color: rgb(77, 85, 93)
+       .remark
+          display: flex
+          padding-top: 18px
+          .block
+            flex: 1
+            text-align: center
+            border-right: 1px solid rgba(7,17,27,0.1)
+            &:last-child
+              border: none
+            h2
+              margin-bottom: 4px
+              line-height: 10px
+              font-size: 10px
+              color: rgb(147, 153, 159)
+            .content
+              line-height: 24px
+              font-size: 10px
+              color: rgb(7, 17, 27)
+              .stress
+                font-size: 24px
+
+    .bulletin
+      padding: 18px 18px 0 18px
+      .title7
+        margin-bottom: 8px
+        line-height: 14px
+        color: rgb(7,17,27)
+        font-size: 14px
+      .content-wrapper
+        padding: 0 12px 16px 12px
+        border-1px(rgba(7, 17, 27, 0.1))
+        .content
+          line-height: 24px
+          font-size: 12px
+          color: rgb(240, 20, 20)
+      .supports
+        .support-item
+          padding: 16px 12px
+          border-1px(rgba(7,17,27,0.1))
+          font-size: 0
+          &:last-child
+             border-none()
+          .icon
+            display: inline-block
+            width: 16px
+            height: 16px
+            vertical-align: top
+            margin-right: 6px
+            background-size: 16px 16px
+            background-repeat: no-repeat
+            &.decrease
+              bg-image('decrease_4')
+            &.discount
+              bg-image('discount_4')
+            &.guarantee
+              bg-image('guarantee_4')
+            &.invoice
+              bg-image('invoice_4')
+            &.special
+              bg-image('special_4')
+          .text
+            line-height: 16px
+            font-size: 12px
+            color: rgb(7, 17, 27)
 
 </style>
